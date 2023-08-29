2af397c0ffcf40e4b025d22a4b67dc0b101246a9
Tudor Nicoara <tudor.nicoara@endava.com>
2023-08-29 16:35:52 +0300

    Created Auction Microservice

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+[Table("Items")]
+public class AuctionDbContext : DbContext
+{
+    public AuctionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Auction> Auctions { get; set; }
+}
