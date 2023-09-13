8ed9981667cd0b99f40a59d4d7b6787310d12b09
Tudor Nicoara <tudor.nicoara@endava.com>
2023-09-13 18:42:22 +0300

    Dockerized all services

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -33,12 +33,12 @@ builder.Services.AddMassTransit(x =>
         //     r.Handle<RabbitMqConnectionException>();
         //     r.Interval(5, TimeSpan.FromSeconds(10));
         // });
-        //
-        // cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
-        // {
-        //     host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-        //     host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
-        // });
+        
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         
         cfg.ConfigureEndpoints(context);
     });
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -24,12 +24,12 @@ builder.Services.AddMassTransit(x =>
         //     r.Handle<RabbitMqConnectionException>();
         //     r.Interval(5, TimeSpan.FromSeconds(10));
         // });
-        //
-        // cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
-        // {
-        //     host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-        //     host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
-        // });
+        
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
