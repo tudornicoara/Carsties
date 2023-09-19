82733cb43d5fba600ea04cc49b36efb2fce1ff51
Tudor Nicoara <tudor.nicoara@endava.com>
2023-09-19 14:12:18 +0300

    Added auction list page + search functionality

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,22 +1,58 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
+import AppPagination from '../components/AppPagination';
+import { Auction, PagedResult } from '@/types';
+import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { shallow } from 'zustand/shallow';
+import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
-async function getData() {
-    const res = await fetch('http://localhost:6001/search?pageSize=10');
+export default function Listings() {
+    const [data, setData] = useState<PagedResult<Auction>>();
+    const params = useParamsStore(state => ({
+        pageNumber: state.pageNumber,
+        pageSize: state.pageSize,
+        searchTerm: state.searchTerm,
+        orderBy: state.orderBy,
+        filterBy: state.filterBy
+    }), shallow)
+    const setParams = useParamsStore(state => state.setParams)
+    const url = qs.stringifyUrl({url: '', query: params})
 
-    if (!res.ok) throw new Error('Failed to fetch data');
+    function setPageNumber(pageNumber: number) {
+        setParams({pageNumber})
+    }
 
-    return res.json();
-}
+    useEffect(() => {
+        getData(url).then(data => {
+            setData(data)
+        })
+    }, [url])
 
-export default async function Listings() {
-    const data = await getData();
+    if (!data) return <h3>Loading...</h3>
 
     return (
-    <div className='grid grid-cols-4 gap-6'>
-        {data && data.results.map((auction: any) => (
-            <AuctionCard auction={auction} key={auction.id} />
-        ))}
-    </div>
+        <>
+            <Filters />
+            {data.totalCount === 0 ? (
+               <EmptyFilter showReset />
+            ) : (
+                <>
+                    <div className='grid grid-cols-4 gap-6'>
+                        {data.results.map(auction => (
+                            <AuctionCard auction={auction} key={auction.id} />
+                        ))}
+                    </div>
+                    <div className='flex justify-center mt-4'>
+                        <AppPagination pageChanged={setPageNumber}
+                            currentPage={params.pageNumber} pageCount={data.pageCount} />
+                    </div>
+                </>
+            )}
+        </>
     )
 }
