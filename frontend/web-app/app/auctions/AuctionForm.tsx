1e3e6aa4022415e3cfca17d03b538264e73d4e53
Tudor Nicoara <tudor.nicoara@endava.com>
2023-10-03 16:23:17 +0300

    Added client CRUD operations

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -3,22 +3,54 @@
 import { Button, TextInput } from 'flowbite-react';
 import React, { useEffect } from 'react'
 import { useForm, FieldValues } from 'react-hook-form'
-import Input from '../components/input';
 import DateInput from '../components/DateInput';
+import { createAuction, updateAuction } from '../actions/auctionActions';
+import { usePathname, useRouter } from 'next/navigation';
+import Input from '../components/Input';
+import toast from 'react-hot-toast/headless';
+import { Auction } from '@/types';
 
-export default function AuctionForm() {
-    const {control, handleSubmit, setFocus,
-        formState: {isSubmitting, isValid, isDirty, errors}} = useForm({
+type Props = {
+    auction?: Auction
+}
+
+export default function AuctionForm({auction}: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+    const {control, handleSubmit, setFocus, reset,
+        formState: {isSubmitting, isValid}} = useForm({
             mode: 'onTouched'
         })
     
     useEffect(() => {
+        if (auction) {
+            const {make, model, color, mileage, year} = auction;
+            reset({make, model, color, mileage, year});
+        }
         setFocus('make')
-    }, [setFocus])
+    }, [auction, reset, setFocus])
 
-    function onSubmit(data: FieldValues) {
-        console.log(data);
-        
+    async function onSubmit(data: FieldValues) {
+        try {
+            let id = '';
+            let res;
+            if (pathname === '/auction/create') {
+                res = await createAuction(data);
+                id = res.id;
+            } else {
+                if (auction) {
+                    res = await updateAuction(data, auction.id);
+                    id = auction.id;
+                }
+            }
+
+            if (res.error) {
+                throw res.error;
+            }
+            router.push(`/auctions/details/${id}`)
+        } catch(error: any) {
+            toast.error(error.status + ' ' + error.message)
+        }
     }
 
   return (
@@ -37,27 +69,30 @@ export default function AuctionForm() {
                 rules={{required: 'Mileage is required'}} />
         </div>
 
-        <Input label='Image URL' name='imageUrl' control={control}
-            rules={{required: 'Image URL is required'}} />
+        {pathname === 'auctions/create' &&
+        <>
+            <Input label='Image URL' name='imageUrl' control={control}
+                rules={{required: 'Image URL is required'}} />
 
-        <div className='grid grid-cols-2 gap-3'>
-            <Input label='Reserve Price (enter 0 if no reserve)'
-                name='reservePrice' control={control} type='number'
-                rules={{required: 'Reserve price is required'}} />
-            <DateInput
-                label='Auction end date/time'
-                name='auctionEnd'
-                control={control}
-                dateFormat='dd MMMM yyyy h:mm a'
-                showTimeSelect
-                rules={{required: 'Auction end date is required'}} />
-        </div>
+            <div className='grid grid-cols-2 gap-3'>
+                <Input label='Reserve Price (enter 0 if no reserve)'
+                    name='reservePrice' control={control} type='number'
+                    rules={{required: 'Reserve price is required'}} />
+                <DateInput
+                    label='Auction end date/time'
+                    name='auctionEnd'
+                    control={control}
+                    dateFormat='dd MMMM yyyy h:mm a'
+                    showTimeSelect
+                    rules={{required: 'Auction end date is required'}} />
+            </div>
+        </>}
 
         <div className='flex justify-between'>
             <Button outline color='gray'>Cancel</Button>
             <Button
             isProcessing={isSubmitting}
-            //disabled={!isValid}
+            disabled={!isValid}
             type='submit'
             outline color='success'>Submit</Button>
         </div>
